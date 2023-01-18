705ce5ed2c37725f7038e2995e648db8bb12894c
Leandro <lean.arrigonni@gmail.com>
2023-01-18 20:16:45 -0300

    acutalice el puerto

--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
-
 	app.Use(cors.New())
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello")
 	})
 
-	app.Listen(":3000")
+	app.Listen(":" + port)
 }
